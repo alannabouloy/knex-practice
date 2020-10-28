b0ea7200ec028f3dab9efc15c3308b6ed12437c8
Alanna Bouloy <lanihope@gmail.com>
2020-10-28 17:25:09 -0400

    added drills in comments

--- src/drills.js
+++ src/drills.js
@@ -5,7 +5,11 @@ const knexInstance = knex({
     client: 'pg',
     connection: process.env.DB_URL
 });
+/*
+1. Get all items that contain text
 
+A function that takes one parameter for searchTerm which will be any string
+The function will query the shopping_list table using Knex methods and select the rows which have a name that contains the searchTerm using a case insensitive match. */
 function searchByString(searchTerm){
     knexInstance
         .select('name')
@@ -16,7 +20,13 @@ function searchByString(searchTerm){
         });
 }
 
-//searchByString('chicken');
+searchByString('chicken');
+
+/* 2. Get all items paginated
+
+A function that takes one parameter for pageNumber which will be a number
+The function will query the shopping_list table using Knex methods and select the pageNumber page of rows paginated to 6 items per page.*/
+
 
 function pagination(pageNumber){
     const productsPerPage = 6;
@@ -32,7 +42,13 @@ function pagination(pageNumber){
         });
 }
 
-//pagination(2);
+pagination(2);
+
+/* 
+3. Get all items added after date
+
+A function that takes one parameter for daysAgo which will be a number representing a number of days.
+This function will query the shopping_list table using Knex methods and select the rows which have a date_added that is greater than the daysAgo.*/
 
 function addedDaysFrom(daysAgo){
     knexInstance
@@ -47,7 +63,13 @@ function addedDaysFrom(daysAgo){
         })
 }
 
-//addedDaysFrom(7);
+addedDaysFrom(7);
+
+/* 
+4. Get the total cost for each category
+
+A function that takes no parameters
+The function will query the shopping_list table using Knex methods and select the rows grouped by their category and showing the total price for each category.*/
 
 function totalPriceForEachCategory(){
     knexInstance
