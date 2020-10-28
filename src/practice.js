0d3582d22dec72b8b17dfc9f2843e2b6af123eb7
Alanna Bouloy <lanihope@gmail.com>
2020-10-28 14:58:22 -0400

    added pagination

--- src/practice.js
+++ src/practice.js
@@ -18,4 +18,17 @@ function searchByProductName(searchTerm) {
 
 searchByProductName('holo');
 
+function paginateProducts(page) {
+    const productsPerPage = 10;
+    const offset = productsPerPage * (page - 1);
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        });
+}
 
+paginateProducts(2);
