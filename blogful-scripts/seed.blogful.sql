63c953cf2766e5b5c334c078c93bf2c73d8e2057
Alanna Bouloy <lanihope@gmail.com>
2020-10-29 20:02:37 -0400

    added blogful scripts

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+insert into blogful_articles(title, content, date_published)
+values
+    ('how to start a dnd campaign', 'all you need to know to get started',                                      now() - '21 days' ::INTERVAL),
+    ('how to build a character FROM SCRATCH', 'all the dos and donts to makeing a compelling character',        now() - '21 days' ::INTERVAL),
+    ('how to choose a class', 'a wizard? a rogue? an artificer? how to choose what works for you',              now() - '20 days' ::INTERVAL),
+    ('how to choose your race', 'goblins, aasimars, and elves OH MY!',                                          now() - '20 days' ::INTERVAL),
+    ('ability score modifiers and what they say about you', 'learn the astology of dnd',                        now() - '19 days' ::INTERVAL),
+    ('baby got backstory?', 'how to develop a compelling backstory for your character',                         now() - '18 days' ::INTERVAL),
+    ('the mercer effect: myth or fact?', 'how has matt mercer raised the bar for DMs eveywhere...',             now() - '18 days' ::INTERVAL),
+    ('ultimate guide to dming', 'how to dm like a boss',                                                        now() - '16 days' ::INTERVAL),
+    ('druids...broken or not? Discuss', 'are druids overpowered? and do we like it that way?',                  now() - '16 days' ::INTERVAL),
+    ('monks man....what the fuck?', 'how to deal with feelings of inadequacy with a monk in your party',        now() - '15 days' ::INTERVAL),
+    ('article 11', 'running out of ideas',                                                                      now() - '14 days' ::INTERVAL),
+    ('evil players: no no or yes yes', 'should i make my chaacter evil? and how do i navigate gameplay?',       now() - '14 days' ::INTERVAL),
+    ('best multiclassing pairs', 'what are the best pairings for multiclass?',                                  now() - '13 days' ::INTERVAL),
+    ('most overpowered spells', 'a list of the most overpowered spells in a spellcasters arsenal',              now() - '12 days' ::INTERVAL),
+    ('paint some minis', 'a step-by-step tutorial on how to paint some minis',                                  now() - '11 days' ::INTERVAL),
+    ('10 best TPKs ever', 'what are the best ways to go out as a party',                                        now() - '9 days'  ::INTERVAL),
+    ('10 worst TPKs ever', 'what are the worst ways to go out as a party',                                      now() - '8 days'  ::INTERVAL),
+    ('so you adopted an NPC...now what?', 'what to do when you find yourself new parent to an NPC',             now() - '7 days'  ::INTERVAL),
+    ('holiday oneshots', 'how to plan the perfect holiday oneshot for your players',                            now()),
+    ('best note-tracker apps out there', 'the best apps for keeping track of your campaign',                    now())
+;
